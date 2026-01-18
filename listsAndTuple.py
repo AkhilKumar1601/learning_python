47b39761cf6cd94fdfc0350b11a21e90a50f4500
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-01-19 05:07:32 +0530

    Adding lists and tuple code

---
+++ listsAndTuple.py
@@ -0,0 +1,52 @@
+friends = ["Chetanya","Pajii",7,False];
+print(friends);
+
+l1 = [1,8,7,2,21,15];
+l1.sort();
+print(l1);
+l1.reverse();
+print(l1);
+l1.append(8);
+print(l1);
+l1.insert(3,8);
+print(l1);
+l1.pop(2);
+print(l1);
+l1.remove(21);
+print(l1);
+
+a = ();
+b = (1,);
+c = (1,2,3);
+print(a, b, c);
+
+d = (1,7,2);
+print(d.count(1));
+print(d.index(1));
+
+fruits = [];
+f1 = input("Enter fruit name");
+fruits.append(f1);
+f2 = input("Enter fruit name");
+fruits.append(f2);
+f3 = input("Enter fruit name");
+fruits.append(f3);
+f4 = input("Enter fruit name");
+fruits.append(f4);
+f5 = input("Enter fruit name");
+fruits.append(f5);
+f6 = input("Enter fruit name");
+fruits.append(f6);
+
+print(fruits);
+
+students_mark = [44,32,55,30,45,43,48];
+students_mark.sort();
+print(students_mark);
+
+print(sum(students_mark));
+
+ab = (7, 0, 8, 0, 0, 9);
+print(ab.count(0));
+
+# t = (1, 2, 3) Modification is not allowed.
